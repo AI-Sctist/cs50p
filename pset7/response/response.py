cfb8ee48a01ee5e300634a2e07b3a84bf0c7362b
AI-Sctist <giahuythcs2008@gmail.com>
2025-08-20 06:34:35 +0000

    feat(pset7): implement problem response

---
+++ pset7/response/response.py
@@ -0,0 +1,12 @@
+import validators
+
+
+def main() -> None:
+    if validators.email(input("What's your email address? ")):
+        print("Valid")
+    else:
+        print("Invalid")
+
+
+if __name__ == "__main__":
+    main()
